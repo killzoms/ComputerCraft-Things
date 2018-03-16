7c5b107b0f46217b32fb5721a7170324d251e289
Blake Redenius <Blayyke@users.noreply.github.com>
2018-03-16 23:40:20 +1000

    Start update checking

--- reactor.lua
+++ reactor.lua
@@ -1,10 +1,11 @@
 os.loadAPI("bccapi")
-
+ 
 if not bccapi then error("BCCAPI not found!", 0) end
-
+ 
 local reactor = bccapi.get("BigReactors-Reactor")
 if not reactor then error("No reactor found!", 0) end
-
+ 
+ 
 --[[  Program Settings  ]]--
 local maxEnergy   -- Max reactor energy before cutoff
 local minEnergy   -- Min reactor energy before start
@@ -14,7 +15,7 @@ local checkUpdate -- Check for updates on startup?
 local loop = true
 local pastebinId = "SJryzsjC"
 --[[         END        ]]--
-
+ 
 local function loadSettings()
   local table = bccapi.fileToTable("rsettings")
   if not table then -- Ensure settings exist
@@ -25,28 +26,37 @@ local function loadSettings()
     table["check-updates"] = true
     bccapi.tableToFile(table, "rsettings")
   end
-
+ 
   maxEnergy = table["max-energy"]
   minEnergy = table["min-energy"]
   waitTime = table["wait-time"]
   checkUpdate = table["check-updates"]
 end
-
+ 
 local function checkForUpdates()
   local headers = { [ "User-Agent" ] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" }
-  local get = http.get("https://pastebin.com/raw/"..waitTime, headers)
-  for i,v in pairs(get) do
-    print(i)
-    print(v)
+  local url = "https://pastebin.com/raw/"..pastebinId
+  if not http.checkURL(url) then
+    error("URL check failed!", 0)
   end
+ 
+  local get = http.get(url, headers)
+  local new = get.readAll()
+  get.close()
+ 
+ 
+ 
+  local old = bccapi.fileToString(shell.getRunningProgram())
+  if old ~= new then error("Not up to date!", 0)
+  else print("Up to date!") end
 end
-
-
+ 
+ 
 local function statusLoop()
   while loop do
     local energy = reactor.getEnergyStored()
-    
-   	if energy > maxEnergy then
+   
+    if energy > maxEnergy then
       reactor.setActive(false)
     elseif energy < minEnergy then
       reactor.setActive(true)
@@ -55,11 +65,11 @@ local function statusLoop()
     os.sleep(waitTime)
   end
 end
-
+ 
 local function other()
 end
-
+ 
 loadSettings()
 if checkUpdate then checkForUpdates() end
-
+ 
 parallel.waitForAll(statusLoop(), other())
